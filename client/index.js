656495ae9d143d28e041b13750f0ef12093b99a0
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-06-02 06:55:58 +0300

    test

--- client/index.js
+++ client/index.js
@@ -1,3 +1,43 @@
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import { getMe, login, register } from './controllers/userController.js';
+// import { addFavoriteFilm, getAllFavorite, removeFIlm } from './controllers/favoriteController.js';
+// import checkAuth from './controllers/checkAuth.js';
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors()); 
+
+
+// dotenv.config();
+
+// app.listen(process.env.PORT, () => {
+//   console.log('сервер запущен');
+// });
+
+
+// mongoose
+//   .connect(process.env.MONGO_SERVER_KEY)
+//   .then(() => {
+//     console.log('mongoose тоже запущен :)');
+//   })
+//   .catch((err) => console.log(err));
+
+// //USER
+// app.post('/register', register);
+// app.post('/login', login);
+// app.get('/me', checkAuth, getMe);
+
+// app.post('/favorite', checkAuth, addFavoriteFilm);
+// app.get('/favorite', checkAuth, getAllFavorite);
+// app.delete('/favorite/:id', checkAuth, removeFIlm);
+
+
+
+
+
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -8,20 +48,26 @@ import checkAuth from './controllers/checkAuth.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 
+// Middleware для перенаправления HTTP на HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
 
 dotenv.config();
 
 app.listen(process.env.PORT, () => {
-  console.log('сервер запущен');
+  console.log('Сервер запущен');
 });
-
 
 mongoose
   .connect(process.env.MONGO_SERVER_KEY)
   .then(() => {
-    console.log('mongoose тоже запущен :)');
+    console.log('Mongoose тоже запущен :)');
   })
   .catch((err) => console.log(err));
 
@@ -33,3 +79,4 @@ app.get('/me', checkAuth, getMe);
 app.post('/favorite', checkAuth, addFavoriteFilm);
 app.get('/favorite', checkAuth, getAllFavorite);
 app.delete('/favorite/:id', checkAuth, removeFIlm);
+
