22046eb6fde01ac0535fb184214ed499869dcc00
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-06-03 23:24:20 +0300

    adaptive

--- frontend/src/components/FullCard/ArtPosters.jsx
+++ frontend/src/components/FullCard/ArtPosters.jsx
@@ -7,24 +7,21 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 const ArtPosters = () => {
-  const {posters} = useSelector((state) => state.movieInfo.movieData);
-  
+  const { posters } = useSelector((state) => state.movieInfo.movieData);
+
   return (
     <div className="">
       <Title text="Изображения из ленты" />
-      <Swiper
-        modules={[Navigation]}
-        spaceBetween={10}
-        slidesPerView={1}
-        navigation
-        // onSlideChange={() => console.log('slide change')}
-        // onSwiper={(swiper) => console.log(swiper)}
-        >
+      <Swiper modules={[Navigation]} spaceBetween={10} slidesPerView={1} navigation>
         {posters.map((item, index) => {
           return (
             <SwiperSlide key={index}>
               <div className="rounded flex items-center justify-center">
-                <img className="h-[900px] object-cover" src={item} alt="poster" />
+                <img
+                  className="h-[900px] object-cover lg:h-[700px] sm:h-[600px] xs:h-[400px] mt:h-[300px]"
+                  src={item}
+                  alt="poster"
+                />
               </div>
             </SwiperSlide>
           );
@@ -35,4 +32,3 @@ const ArtPosters = () => {
 };
 
 export default ArtPosters;
-
--- frontend/src/components/FullCard/FilmVideo.jsx
+++ frontend/src/components/FullCard/FilmVideo.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import Title from '../common/Title';
 import ReactPlayer from 'react-player/youtube';
@@ -10,25 +11,39 @@ import 'swiper/css/navigation';
 
 const FilmVideo = () => {
   const { videos } = useSelector((state) => state.movieInfo.movieData);
+  const [screenVideo, setScreenVideo] = useState('');
+  useEffect(() => {
+    const updateSlidesPerView = () => {
+      if (window.innerWidth < 580) {
+        setScreenVideo('400px');
+      } else if (window.innerWidth < 768) {
+        setScreenVideo('500px');
+      } else if (window.innerWidth < 992) {
+        setScreenVideo('600px');
+      } else if (window.innerWidth < 1060) {
+        setScreenVideo('700px');
+      }else{ 
+        setScreenVideo('900px')
+      }
+    };
+    window.addEventListener('resize', updateSlidesPerView);
+    updateSlidesPerView();
 
+    return () => {
+      window.removeEventListener('resize', updateSlidesPerView);
+    };
+  }, []);
   return (
     <div className="">
       <Title text="Фрагменты видео и трейлеры" />
       {videos.length > 0 ? (
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={10}
-          slidesPerView={1}
-          navigation
-          // onSlideChange={() => console.log('slide change')}
-          // onSwiper={(swiper) => console.log(swiper)}
-          >
+        <Swiper modules={[Navigation]} spaceBetween={10} slidesPerView={1} navigation>
           {videos.map((item, index) => {
             const { url, name } = item;
             return (
               <SwiperSlide key={index} className="w-[1000px]">
                 <div className="flex justify-center flex-col items-center">
-                  <ReactPlayer url={url} controls={true} width="100%" height="800px" />
+                  <ReactPlayer url={url} controls={true} width='100%' height={screenVideo} />
                   <p>{name}</p>
                 </div>
               </SwiperSlide>
