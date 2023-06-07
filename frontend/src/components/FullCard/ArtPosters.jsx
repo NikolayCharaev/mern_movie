43383affa25f089519af8334e60b771242b69758
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-06-07 07:32:08 +0300

    add Skeleton

--- frontend/src/components/FullCard/ArtPosters.jsx
+++ frontend/src/components/FullCard/ArtPosters.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Title from '../common/Title';
@@ -6,7 +7,11 @@ import { Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
 const ArtPosters = () => {
+  const [posterLoad, setPosterLoad] = useState(true);
   const { posters } = useSelector((state) => state.movieInfo.movieData);
 
   return (
@@ -17,10 +22,16 @@ const ArtPosters = () => {
           return (
             <SwiperSlide key={index}>
               <div className="rounded flex items-center justify-center">
+                {posterLoad && (
+                  <Skeleton className="h-[900px] w-[1300px]  object-cover lg:h-[700px] sm:h-[600px] xs:h-[400px] mt:h-[300px]" />
+                )}
                 <img
-                  className="h-[900px] object-cover lg:h-[700px] sm:h-[600px] xs:h-[400px] mt:h-[300px]"
+                  className={`h-[900px] object-cover lg:h-[700px] sm:h-[600px] xs:h-[400px] mt:h-[300px] ${
+                    posterLoad ? 'hidden' : 'block'
+                  }`}
                   src={item}
                   alt="poster"
+                  onLoad={() => setPosterLoad(false)}
                 />
               </div>
             </SwiperSlide>
