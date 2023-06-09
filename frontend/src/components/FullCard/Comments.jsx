f5c81434a0f5a4724f1f232a5faa161ab2a6033d
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-06-09 19:38:36 +0300

    add toast to commenting

--- frontend/src/components/FullCard/Comments.jsx
+++ frontend/src/components/FullCard/Comments.jsx
@@ -2,8 +2,8 @@ import {useEffect} from 'react';
 import Button from '../common/Button';
 import Title from '../common/Title';
 import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
-import dayjs from 'dayjs';
 import { fetchAddComment, fetchAllComments } from '../../redux/comments/commentSlice';
 
 import { useForm } from 'react-hook-form';
@@ -12,6 +12,7 @@ const Comments = () => {
   const dispatch = useDispatch();
 
   const { kinopoiskId } = useSelector((state) => state.movieInfo.movieData.film);
+  const {userData} = useSelector(state =>  state.userSlice.user)
   const { items } = useSelector((state) => state.comments.allComments);
   const {
     register,
@@ -21,6 +22,9 @@ const Comments = () => {
   } = useForm();
 
   const OnSubmit = (data) => {
+    if (!userData){
+        return toast.error('авторизируйтесь для комментирования фильмов')
+    }
     const { text } = data;
     dispatch(fetchAddComment({ comment: text, id: kinopoiskId }));
     dispatch(fetchAllComments(kinopoiskId))
@@ -29,18 +33,23 @@ const Comments = () => {
   };
 
 
+//   useEffect(() => { 
+//     dispatch(fetchAllComments(kinopoiskId))
+//   },[items])
+
 
   return (
     <div>
       <Title text={'Комментарии к фильму'} />
       <div className="overflow-scroll max-h-[400px]">
+        {items.length <=0 && <p>Тут пока ничего нет, оставьте комментарий первым :)</p>}
         {items.map((comment) => {
           const { text } = comment;
           const { email, username } = comment.userInfo;
           return (
             <>
               <div className='p-2 border border-blue-800 rounded mb-5 relative '>
-                <div className='absolute top-2 left-2 text-xs'>
+                <div className='absolute top-2 left-2 text-xs flex gap-4'>
                     <p>{username}</p>
                 </div>
                 <p className='text-sm mt-10 p-2 bg-headerBg'>{text}</p>
